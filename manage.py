be98456212e70814d1c6c68477db1c6c4ba127ca
ruitian <chinabingwei@163.com>
2015-08-03 14:48:21 +0800

    add manage

---
+++ manage.py
@@ -0,0 +1,35 @@
+from lib import app, db
+from flask.ext.script import Manager, Server, Shell
+
+from lib.models import UserModel
+
+manager = Manager(app)
+
+
+def make_shell_context():
+    return dict(
+        app=app,
+        db=db,
+        UserModel=UserModel
+    )
+
+manager.add_command("runserver", Server(
+    use_debugger=True,
+    use_reloader=True,
+    host="0.0.0.0",
+    port="3000"
+    )
+)
+manager.command("shell", Shell(
+    make_context=make_shell_context
+    )
+)
+
+
+@manager.command
+def deploy():
+    pass
+
+
+if __name__ == '__main__':
+    manager.run()
