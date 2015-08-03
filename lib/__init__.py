278484d1c90dbeff30b0fd6c439a97f784bf39bc
ruitian <chinabingwei@163.com>
2015-08-03 15:09:39 +0800

    add _init__.py

---
+++ lib/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import os
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+from flask.ext.login import LoginManager
+from .config import config
+
+
+app = Flask(__name__)
+db = MongoEngine()
+login_manager = LoginManager()
+
+
+with app.app_context():
+    config_name = os.getenv('FLASK_CONFIG') or 'default'
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth/login'
+
+    from .views import(
+        bp_auth
+    )
+
+    app.register_blueprint(
+        bp_auth,
+        url_prefix='/auth'
+    )
