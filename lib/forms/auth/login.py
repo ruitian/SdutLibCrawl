bf898a7b047fef038d50f6d716aec35018dbaa66
ruitian <chinabingwei@163.com>
2015-08-03 23:47:00 +0800

    add LoginForm

---
+++ lib/forms/auth/login.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Required
+
+from lib.models import UserModel
+
+
+class LoginForm(Form):
+    username = StringField(
+        'Username',
+        [
+            Required(),
+        ]
+    )
+    password = PasswordField('Password', [Required()])
+    submit = SubmitField('Login')
+
+    def login(self):
+        return UserModel.create_user(
+            username=self.username.data,
+            password=self.password.data
+        )
