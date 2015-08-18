6e04ca0924d8ef962a8b89f6b74872cd2db0524f
ruitian <chinabingwei@163.com>
2015-08-18 18:56:28 +0800

    auth login

--- lib/views/__init__.py
+++ lib/views/__init__.py
@@ -1,2 +1,3 @@
 from .index import bp_index  # noqa
 from .libr import bp_libr  # noqa
+from .auth import bp_auth  # noqa
--- lib/views/index/index.py
+++ lib/views/index/index.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from flask import render_template
-from lib.forms import LoginForm  # noqa
 
 from flask.views import MethodView
 
